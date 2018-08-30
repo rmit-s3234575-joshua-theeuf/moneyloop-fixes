572abb99100525921a6fd54d0414308eee682d40
deepozzzie <s3234575@student.rmit.edu.au>
2018-08-30 12:14:54 +1000

    remove

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require turbolinks
 //= require jquery-3.2.1.min.js
 //= require jquery.main.js

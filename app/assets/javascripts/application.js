704a679788e0451650f399976a86e3af7e56bd06
deepozzzie <s3234575@student.rmit.edu.au>
2018-08-30 12:17:58 +1000

    jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery-3.2.1.min.js
-//= require jquery.main.js
+
 //= require jquery
 //= require jquery_ujs
+//= require jquery.main.js
 //= require turbolinks
 //= require_tree .
